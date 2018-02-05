2a6110dcec9924e73c4fe3165c7d971d0e3f0e13
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-02-05 17:33:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <tutorial-list></tutorial-list>
+  </div>
+</template>
+
+<script>
+  import TutorialList from './components/TutorialList.vue'
+
+export default {
+  name: 'App',
+  components: { TutorialList }
+}
+</script>
---
+++ src/components/TutorialList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h2>Tutorial list</h2>
+
+        <table class="table">
+            <thead class="thead-inverse">
+            <tr>
+                <th>#</th>
+                <th>{{ test }}</th>
+                <th>Last Name</th>
+                <th>Username</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TutorialsList",
+        data() {
+            return {
+                test: 'test'
+            }
+        },
+
+        beforeMount() {
+            fetchData()
+        },
+
+        methods: {
+            fetchData(url) {
+                console.log()
+            }
+        }
+    }
+</script>
